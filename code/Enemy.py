0504997b5d98d48ce08113bc4b9d37ec4852a3e3
RuanSchaefer <ruanschaefer@hotmail.com>
2024-08-31 15:47:54 -0300

    project

---
+++ code/Enemy.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY, WIN_HEIGHT
+from code.EnemyShot import EnemyShot
+from code.Entity import Entity
+
+class Enemy(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+
+    def move(self):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+
+    def shoot(self):
+        self.shot_delay -= 1
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+
+
+class Enemy3(Enemy):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+        self.direction_y = 1
+        self.speed_y = ENTITY_SPEED[self.name]
+
+    def move(self):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+
+        self.rect.centery += self.speed_y * self.direction_y
+
+        if self.rect.bottom >= WIN_HEIGHT:
+            self.direction_y = -1
+            self.speed_y = ENTITY_SPEED[self.name]
+
+        elif self.rect.top <= 0:
+            self.direction_y = 1
+            self.speed_y = ENTITY_SPEED[self.name] * 2
+
+    def shoot(self):
+        self.shot_delay -= 1
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
